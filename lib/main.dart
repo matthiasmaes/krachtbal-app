0dc508643b41612aeaffbea1c7f0c1305aee9313
Matthias Maes <matthias.maes@outlook.com>
2022-03-18 13:49:38 +0100

    Remove future builder and added splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'localpersitance.dart';
 import 'widgets_calendar.dart';
 import 'widgets_ranking.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,123 +38,119 @@ class MyApp extends StatelessWidget {
         primarySwatch: colorCustom,
         fontFamily: 'Montserrat',
       ),
-      home: const MyHomePage(),
+      // home: const MyHomePage(),
+      home: Expanded(
+        child: AnimatedSplashScreen.withScreenFunction(
+          backgroundColor: const Color.fromARGB(255, 210, 61, 41),
+          duration: 0,
+          splash: 'assets/images/splash.png',
+          splashIconSize: 300.0,
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.fade,
+          screenFunction: () async {
+            http.Response rankingResponse = await http.get(
+              Uri.parse(
+                'https://matthiasmaes.com/krachtbal/scraped_data/ranking/latest.json',
+              ),
+            );
+
+            http.Response calendarResponse = await http.get(
+              Uri.parse(
+                'https://matthiasmaes.com/krachtbal/scraped_data/calendar/latest.json',
+              ),
+            );
+
+            return MyHomePage(
+                rankingData: json.decode(rankingResponse.body),
+                calendarData: json.decode(calendarResponse.body));
+          },
+        ),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage(
+      {Key? key, required this.rankingData, required this.calendarData})
+      : super(key: key);
+
+  final Map<String, dynamic> rankingData;
+  final Map<String, dynamic> calendarData;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> pageTitle = ["Rangschikking", "Kalender"];
-  List<String> urls = [
-    'https://matthiasmaes.com/krachtbal/scraped_data/ranking/latest.json',
-    // 'https://matthiasmaes.com/krachtbal/scraped_data/calendar/latest.json'
-    'https://matthiasmaes.com/krachtbal/scraped_data/calendar/16-03-2022-22-01-11-krachtbal-scraped.json'
-  ];
   int index = 0;
-
+  List<String> pageTitle = ["Rangschikking", "Kalender"];
   List<String> englishPageTitles = ["Ranking", "Calendar"];
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: ((context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          http.Response responseObject = snapshot.data as http.Response;
-          Map<String, dynamic> parsedData = json.decode(responseObject.body);
-          return Scaffold(
-            backgroundColor: const Color.fromARGB(255, 219, 219, 219),
-            bottomNavigationBar: BottomNavigationBar(
-                currentIndex: index,
-                onTap: (int index) {
-                  setState(() {
-                    this.index = index;
-                  });
-                },
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.calendar_today),
-                    label: 'Calendar',
-                  ),
-                ]),
-            body: CustomScrollView(slivers: [
-              SliverAppBar(
-                pinned: true,
-                expandedHeight: 200,
-                flexibleSpace: FlexibleSpaceBar(
-                  titlePadding: EdgeInsets.only(bottom: 15, left: 15),
-                  title: Text(pageTitle[index]),
-                  background: const Image(
-                    image: AssetImage('assets/images/Picture1.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.only(top: 25),
-                sliver: SliverList(
-                  delegate: englishPageTitles[index] == 'Ranking'
-                      ? SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            return CustomWidgetCardsRanking(
-                              data:
-                                  parsedData[parsedData.keys.elementAt(index)],
-                              title: parsedData.keys.elementAt(index),
-                              favorite: LocalPersitance().getFavorite(),
-                            );
-                          },
-                          childCount: parsedData.keys.length,
-                        )
-                      : SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            return CustomWidgetCardsCalender(
-                              data:
-                                  parsedData[parsedData.keys.elementAt(index)],
-                              title: parsedData.keys.elementAt(index),
-                              favorite: LocalPersitance().getFavorite(),
-                            );
-                          },
-                          childCount: parsedData.keys.length,
-                        ),
-                ),
-              )
-            ]),
-          );
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: SizedBox(
-              width: double.infinity,
-              height: double.infinity,
-              child: Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  child: CircularProgressIndicator(),
-                  width: 50,
-                  height: 50,
-                ),
-              ),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 219, 219, 219),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (int index) {
+            setState(() {
+              this.index = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          );
-        } else {
-          return const Text('Something went wrong');
-        }
-      }),
-      future: http.get(
-        Uri.parse(
-          urls[index],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Calendar',
+            ),
+          ]),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: const EdgeInsets.only(bottom: 15, left: 15),
+            title: Text(pageTitle[index]),
+            background: const Image(
+              image: AssetImage('assets/images/Picture1.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 25),
+          sliver: SliverList(
+            delegate: englishPageTitles[index] == 'Ranking'
+                ? SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return CustomWidgetCardsRanking(
+                        data: widget.rankingData[
+                            widget.rankingData.keys.elementAt(index)],
+                        title: widget.rankingData.keys.elementAt(index),
+                        favorite: LocalPersitance().getFavorite(),
+                      );
+                    },
+                    childCount: widget.rankingData.keys.length,
+                  )
+                : SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return CustomWidgetCardsCalender(
+                        data: widget.calendarData[
+                            widget.calendarData.keys.elementAt(index)],
+                        title: widget.calendarData.keys.elementAt(index),
+                        favorite: LocalPersitance().getFavorite(),
+                      );
+                    },
+                    childCount: widget.calendarData.keys.length,
+                  ),
+          ),
+        )
+      ]),
     );
   }
 }
